88b14465b82d887da77a3e4fd41ea953bbc82f6a
fjavierm <javier.marquez@digital.cabinet-office.gov.uk>
2018-01-09 13:56:29 +0000

    Lambda to consume messages from SNS

---
+++ src/main/java/uk/gov/lambda/updater/LogEvent.java
@@ -0,0 +1,22 @@
+package uk.gov.lambda.updater;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class LogEvent implements RequestHandler<SNSEvent, Object> {
+
+    public Object handleRequest(SNSEvent request, Context context) {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        context.getLogger().log("Invocation started: " + timeStamp);
+
+        context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
+
+        timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        context.getLogger().log("Invocation completed: " + timeStamp);
+        return null;
+    }
+}
