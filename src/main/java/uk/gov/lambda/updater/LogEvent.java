bca908c08f61b28b7620774753cd681c261e91dd
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-01-10 10:49:07 +0000

    wip add slack post

--- src/main/java/uk/gov/lambda/updater/LogEvent.java
+++ src/main/java/uk/gov/lambda/updater/LogEvent.java
@@ -4,14 +4,48 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import com.google.gson.Gson;
 
 public class LogEvent implements RequestHandler<SNSEvent, Object> {
 
     public Object handleRequest(SNSEvent request, Context context) {
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
         context.getLogger().log("Invocation started: " + timeStamp);
+
+        class SlackJson {
+            String text = request.getRecords().get(0).getSNS().getMessage();
+        }
+        SlackJson slackJson = new SlackJson();
+
+        String postUrl = System.getenv("HOOK_URL");// put in your url
+        context.getLogger().log(postUrl);
+        Gson gson = new Gson();
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(postUrl);
+        StringEntity postingString = null;//gson.tojson() converts your pojo to json
+        try {
+            postingString = new StringEntity(gson.toJson(slackJson));
+        } catch (UnsupportedEncodingException e) {
+            context.getLogger().log(e.getMessage() + 'IN ');
+        }
+        post.setEntity(postingString);
+        post.setHeader("Content-type", "application/json");
+        try {
+            HttpResponse response = httpClient.execute(post);
+        } catch (IOException e) {
+            context.getLogger().log(e.getMessage());
+        }
 
         context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
 
